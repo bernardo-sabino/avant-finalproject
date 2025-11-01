d3671c3f51ed5d2749fcefa4875e5f153a0e8a25
Bernardo Sabino <contatobernardosabino@gmail.com>
2025-11-01 16:18:43 -0300

    feat: camada de processamento de imagem

--- src/vant_vision_pkg/vant_vision_pkg/vision.py
+++ src/vant_vision_pkg/vant_vision_pkg/vision.py
@@ -10,6 +10,7 @@ from cv_bridge import CvBridge # Ponte para converter imagem ROS -> Imagem Open 
 
 # Interfaces
 from sensor_msgs.msg import Image 
+from std_msgs.msg import Float32  
 
 # Importação de módulos padrões 
 import time
@@ -30,11 +31,19 @@ class VisionNode(Node):
             history=HistoryPolicy.KEEP_LAST, # O nó só armazenerá em seu buffer um número fixo das mensagens mais rescentes
 
 
-            depth=10 # Esse é o número fixo de mensagens armazenadas no buffer
+            depth=1 # Esse é o número fixo de mensagens armazenadas no buffer
         )
 
         self.bridge = CvBridge()   # Inicilizando a ponte (CVBridge)
         self.imagem_recebida = False # Flag para saber se a imagem foi recebida
+
+        # Dimensões da imagem (definidas no primeiro frame)
+        self.imagem_largura = 0
+        self.imagem_altura = 0
+        self.imagem_centro = 0
+
+        # Publisher
+        self.error_pub = self.create_publisher(Float32, '/line_follower/error', 10)
 
         # Subscriber
         self.imagem_sub = self.create_subscription(
@@ -50,13 +59,49 @@ class VisionNode(Node):
     def imagem_callback(self, msg):
 
         """
-        Método chamado a cada frame da imagem carregado
+        Método chamado a cada frame da imagem carregado, é o lugar que todo processamento de imagem ocorre
         """
+
+        # Conversão de Imagem ROS2 para Imagem OpenCV
         try:
             frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")      
             self.imagem_recebida = True
         except Exception as e:
             self.get_logger().error(f"Falha ao converter a imagem: {e}")
+
+        # Definição das dimensões
+        if self.imagem_largura == 0:
+            h,w,d = frame.shape
+            self.imagem_largura = w
+            self.imagem_altura = h
+            self.imagem_centro = w // 2 
+
+        # Processamento de imagem
+
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Converter para HSV
+
+        lower_blue = np.array([100, 150, 50]) # Definir o range da cor Azul (lower)
+        upper_blue = np.array([140, 255, 255]) # Definir o range da cor Azul (upper)
+
+        mask = cv2.inRange(hsv_frame, lower_blue, upper_blue) # Cria a máscara
+
+        M = cv2.moments(mask) # Encontrar o Centro da Linha (Centróide)
+
+        error_msg = Float32() # Cria a mensagem de erro
+        
+        if M["m00"] > 0: # A linha foi detectada
+
+            cx = int(M["m10"] / M["m00"]) # Calcula o centro (cx) da linha na imagem
+             
+            error = self.imagem_centro - cx   # Erro = Posição do Centro da Imagem - Posição do Centro da Linha
+            
+            error_msg.data = float(error)
+        
+        else:   # A linha não foi detectada
+            error_msg.data = 0.0 
+        
+        self.error_pub.publish(error_msg) # Publicando o erro para manipulação por parte do nó de navegação
+
 
 # ===== Main =====
 def main():
@@ -73,3 +118,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
